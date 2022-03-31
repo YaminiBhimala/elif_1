3b5a9966019bb9c6524428edaffe0582df464384
yamini <yamini.svecw@gmail.com>
2022-03-31 00:47:57 -1100

    first time with github

---
+++ if_else_1.py
@@ -0,0 +1,17 @@
+# this is my first if else program
+'''
+  Developer name:  AAAA
+  date :  30th March 2022
+'''
+
+age=int(input('enter the age '))
+if age > 59:
+   print ('retired ')
+   print ('Have a nice retired life ')
+else:
+   print ('working ....')
+   print ('wish you success in career ')
+
+print ('thank you for executing this if ...else code ... ')
+
+
